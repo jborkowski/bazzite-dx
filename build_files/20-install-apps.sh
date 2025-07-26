3d634501ed54c67a1cdc936a1cce21d9ecd48d02
Jonatan Borkowski <jonatan@thebo.me>
2025-07-26 22:17:03 +0200

    Revert "Merge pull request #111 from ublue-os/simplify-builds"
    This reverts commit 5ae166bef754e340cb58d80f6e6467f1af8eb68e, reversing
    changes made to 2e76a46f046ffc6b2aefb46804c266bd0f32299e.

--- build_files/20-install-apps.sh
+++ build_files/20-install-apps.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/bash
 set -xeuo pipefail
 
+mkdir -p /usr/share/gamescope-session-plus/
+curl -Lo /usr/share/gamescope-session-plus/bootstrap_steam.tar.gz https://large-package-sources.nobaraproject.org/bootstrap_steam.tar.gz
+dnf5 install --enable-repo="copr:copr.fedorainfracloud.org:bazzite-org:bazzite" -y \
+    gamescope-session-plus \
+    gamescope-session-steam
+
 dnf5 install -y \
     android-tools \
     bcc \
@@ -26,27 +32,27 @@ systemctl enable input-remapper.service
 systemctl enable uupd.timer
 
 # Remove -deck specific changes to allow for login screens
-rm -f /etc/sddm.conf.d/steamos.conf
-rm -f /etc/sddm.conf.d/virtualkbd.conf
-rm -f /usr/share/gamescope-session-plus/bootstrap_steam.tar.gz
-systemctl disable bazzite-autologin.service
+# rm -f /etc/sddm.conf.d/steamos.conf
+# rm -f /etc/sddm.conf.d/virtualkbd.conf
+# rm -f /usr/share/gamescope-session-plus/bootstrap_steam.tar.gz
+# systemctl disable bazzite-autologin.service
 
-if [[ "$IMAGE_NAME" == *gnome* ]]; then
-    # Remove SDDM and re-enable GDM on GNOME builds.
-    dnf5 remove -y \
-        sddm
+# if [[ "$IMAGE_NAME" == *gnome* ]]; then
+#     # Remove SDDM and re-enable GDM on GNOME builds.
+#     dnf5 remove -y \
+#         sddm
 
-    systemctl enable gdm.service
-else
-    # Re-enable logout and switch user functionality in KDE
-    sed -i -E \
-      -e 's/^(action\/switch_user)=false/\1=true/' \
-      -e 's/^(action\/start_new_session)=false/\1=true/' \
-      -e 's/^(action\/lock_screen)=false/\1=true/' \
-      -e 's/^(kcm_sddm\.desktop)=false/\1=true/' \
-      -e 's/^(kcm_plymouth\.desktop)=false/\1=true/' \
-      /etc/xdg/kdeglobals
-fi
+#     systemctl enable gdm.service
+# else
+#     # Re-enable logout and switch user functionality in KDE
+#     sed -i -E \
+#       -e 's/^(action\/switch_user)=false/\1=true/' \
+#       -e 's/^(action\/start_new_session)=false/\1=true/' \
+#       -e 's/^(action\/lock_screen)=false/\1=true/' \
+#       -e 's/^(kcm_sddm\.desktop)=false/\1=true/' \
+#       -e 's/^(kcm_plymouth\.desktop)=false/\1=true/' \
+#       /etc/xdg/kdeglobals
+# fi
 
 
 dnf5 install --enable-repo="copr:copr.fedorainfracloud.org:ublue-os:packages" -y \
