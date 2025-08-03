48d9067be3dc6d8a7def4316856d718918d82539
Jonatan Borkowski <jonatan@thebo.me>
2025-08-03 21:58:36 +0200

    Update 20-install-apps.sh

--- build_files/20-install-apps.sh
+++ build_files/20-install-apps.sh
@@ -5,7 +5,8 @@ mkdir -p /usr/share/gamescope-session-plus/
 curl -Lo /usr/share/gamescope-session-plus/bootstrap_steam.tar.gz https://large-package-sources.nobaraproject.org/bootstrap_steam.tar.gz
 dnf5 install --enable-repo="copr:copr.fedorainfracloud.org:bazzite-org:bazzite" -y \
     gamescope-session-plus \
-    gamescope-session-steam
+    gamescope-session-steam \
+    unl0kr
 
 dnf5 install -y \
     android-tools \
